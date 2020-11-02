122f28a2705d0681516e1c9d3f95ce0cd03d73ff
ANONYMOUS <73218912+DEADLY-HACKER-ANONYMOUS@users.noreply.github.com>
2020-11-02 09:53:15 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,22 @@
+function AIYY() {
+
+    var number = prompt("FOR WHICH NUMBER U WANT UPTO 2000TH TABLE ??")
+   
+    for(let x=0; x<=20; x++){
+
+       var c = number*x
+      console.log(c)
+    }
+    
+
+        
+       
+
+
+   
+    }
+    
+
+    
+    AIYY();
+    
